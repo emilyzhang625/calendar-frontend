94b6620658c7fcdccf06e851acdc92a1e1609659
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-07-06 05:27:25 -0700

    Test.jsx is properly performing as add form

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,13 +1,43 @@
 import itemService from "../items";
+import { useState } from "react";
 
 function Test() {
-  const handleClick = () => {
-    const newItem = { name: "testing" };
+  const [showAddButton, setShowAddButton] = useState(true);
+  const [newItem, setNewItem] = useState({ name: "" });
+
+  const handleSubmit = () => {
+    event.preventDefault;
     console.log(newItem);
     itemService.addItem(newItem);
+    setNewItem({ name: "" });
   };
 
-  return <button onClick={handleClick}>Test</button>;
+  const handleChange = (event) => {
+    setNewItem({ name: event.target.value });
+  };
+
+  const handleClick = (props) => {
+    setShowAddButton(props);
+  };
+
+  return (
+    <div>
+      {showAddButton && <button onClick={() => handleClick(false)}>+</button>}
+      {!showAddButton && (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="item name"
+              onChange={handleChange}
+            ></input>
+            <button type="submit">Add item</button>
+          </form>
+          <button onClick={() => handleClick(true)}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Test;
